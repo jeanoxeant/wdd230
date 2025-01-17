ce5f283212104783261e76b43de307aae349297c
jeanoxeant <oxe24001@byui.edu>
2025-01-17 04:50:49 -0400

    update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -103,3 +103,18 @@ const courses = [
       completed: false
   }
 ]
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("🕶️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "🔆";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "🕶️";
+	}
+});
