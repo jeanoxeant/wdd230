8b1952ee48762f4f8562431e1e02e212333b0deb
jeanoxeant <oxe24001@byui.edu>
2025-01-22 07:03:32 -0400

    chamber update 6

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -6,16 +6,6 @@ let oLastModif = new Date(document.lastModified);
 
 lastModified.innerHTML = `<span class="highlight">${oLastModif.toLocaleDateString()} ${oLastModif.toLocaleTimeString()}</span>`;
 
-/*function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-}
-*/
-
 // Add a click event listender to the hamburger button and use a callback function that toggles the list element's list of classes.
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
@@ -23,20 +13,4 @@ const navigation = document.querySelector('.navigation');
 hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
-});
-
-// Dark mode
-const modeButton = document.querySelector("#mode");
-const main = document.querySelector("main");
-
-modeButton.addEventListener("click", () => {
-	if (modeButton.textContent.includes("🕶️")) {
-		main.style.background = "#000";
-		main.style.color = "#fff";
-		modeButton.textContent = "🔆";
-	} else {
-		main.style.background = "#eee";
-		main.style.color = "#000";
-		modeButton.textContent = "🕶️";
-	}
 });
