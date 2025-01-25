138b7bd88a7f6f05e90f044541fc74e9994404a7
jeanoxeant <oxe24001@byui.edu>
2025-01-25 04:13:24 -0400

    lazy loading update

---
+++ week03/scripts/lazyload.js
@@ -0,0 +1,8 @@
+// use the date object
+const today = new Date();
+
+currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+let oLastModif = new Date(document.lastModified);
+
+lastModified.innerHTML = `<span class="highlight">${oLastModif.toLocaleDateString()} ${oLastModif.toLocaleTimeString()}</span>`;
