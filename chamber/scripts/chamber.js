af594b6a15e01ecfd87f0be36c2b84817f87977b
jeanoxeant <oxe24001@byui.edu>
2025-02-06 07:48:15 -0400

    discover update

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -17,35 +17,29 @@ hamButton.addEventListener('click', () => {
 
 // Visit Count
 // ********* Page Visits Counter *********
-function updatePageVisitsCounter() {
-    const msToDays = 84600000;
-    let lastVisit = window.localStorage.getItem("numDiscoverPage");
-    const visitsDisplay = document.querySelector(".latest-visit");
 
-    // Calculate the time difference between visits
-    var currentTime = new Date();
-    var previousVisit = new Date(lastVisit);
-    var timeDifference = currentTime.getTime() - previousVisit.getTime();
-    var daysDifference = timeDifference / msToDays;
+const message = document.querySelector("#message");
+const now = new Date();
+const lastVisit = localStorage.getItem("lastVisit");
 
-    // Display message based on time difference
-    if (lastVisit != null) {
-        if (daysDifference > 0 && daysDifference < 1) {
-            visitsDisplay.textContent = `Back so soon! Awesome!`;
-        } else if (Math.floor(daysDifference) === 1) {
-            visitsDisplay.textContent = `You last visited 1 day ago.`;
-        } else {
-            visitsDisplay.textContent = `You last visited ${Math.floor(daysDifference)} days ago.`;
-        }
-    } else {
-        visitsDisplay.textContent = `Welcome! Let us know if you have any questions.`;
-    }
-    // store the new visit count total into localStorage
-    localStorage.setItem("numDiscoverPage", new Date());
-
-    // console.log("Current Time: " + currentTime);
-    // console.log("Previous Visit: " + previousVisit);
-    // console.log("Time Difference: " + timeDifference);
-    // console.log("Days Difference: " + daysDifference);
+if (!lastVisit) {
+    message.textContent = "Welcome! Let us know if you have any questions.";
 }
 
+else {
+    const lastVisitDate = new Date(lastVisit);
+    const diffTime = now - lastVisitDate;
+    const diffDays = Math.floor(diffTime / (1000 * 60 * 24));
+
+    if (diffDays === 0) {
+        message.textContent = "Back so soon! Awesome!";
+        
+    }else if (diffDays === 1) {
+        message.textContent = "You last visited 1 day ago.";
+    }else {
+        message.textContent = `You last visited ${diffDays} days ago.`;
+    }
+}
+
+// store the new visit count total into localStorage
+localStorage.setItem("lastVisit", now.toDateString());
