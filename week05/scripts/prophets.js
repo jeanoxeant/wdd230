5a3aae4e692b2fb4733521c558ca7c25c9f171e6
jeanoxeant <oxe24001@byui.edu>
2025-02-15 21:00:42 -0400

    Api update

--- week05/scripts/prophets.js
+++ week05/scripts/prophets.js
@@ -1,23 +1,23 @@
 const url = 'https://brotherblazzard.github.io/canvas-content/latter-day-prophets.json';
 const cards = document.querySelector('#cards');
 
-async function getProphetData(url) {
+async function getProphetData() {
     const response = await fetch(url);
     const data = await response.json();
     //console.table(data.prophets);
     displayProphets(data.prophets); // note that we reference the prophets array of the JSON data object, not just the object
-  }
+}
 
   
-    const displayProphets = (prophets) => {
-        prophets.forEach((prophet) => {
-        // Create elements to add to the div.cards element
-        let card = document.createElement('section');
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+         // Create elements to add to the div.cards element
+         let card = document.createElement('section');
         let fullName = document.createElement('h2'); // fill in the blank
         let portrait = document.createElement('img');
         let birthDate = document.createElement('h4');
         let birthPlace = document.createElement('h4');
-    
+        
         // Build the h2 content out to show the prophet's full name
         fullName.textContent = `${prophet.name} ${prophet.lastname}`; // fill in the blank
         // Build the image portrait by setting all the relevant attributes
@@ -29,15 +29,14 @@ async function getProphetData(url) {
 
         birthDate.textContent = `Date of birth: ${prophet.birthdate}`;
         birthPlace.textContent = `Place of birth : ${prophet.birthplace}`;
-    
+        
         // Append the section(card) with the created elements
         card.appendChild(fullName); //fill in the blank
         card.appendChild(birthDate);
         card.appendChild(birthPlace);
         card.appendChild(portrait);
-    
+        
         cards.appendChild(card);
-        }); // end of arrow function and forEach loop
-    }
-
+    }); // end of arrow function and forEach loop
+}
 getProphetData();
