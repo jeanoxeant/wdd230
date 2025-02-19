46e2dbf44b91cd1e8661a703db8fa2a0b8f81455
jeanoxeant <oxe24001@byui.edu>
2025-02-18 21:25:13 -0400

    media query update

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -65,4 +65,39 @@ localStorage.setItem("numVisits-ls", numVisits);
 // 💡A client can view the localStorage data using the Applications panel in the browsers's DevTools - check it out on any major site.
 
 
+//Current weather
 
+//select HTML elements in the document
+const currentTemp = document.querySelector('#current-temp');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('#figcaption');
+
+const url = 'https://api.openweathermap.org/data/2.5/weather?lat=18.98&lon=-69.05&appid=82b9407b7b6d113e077d354c4b29fe74&units=imperial';
+
+
+
+async function apiFetch(){
+    try{
+        const response = await fetch(url);
+        if(response.ok){
+            const data = await response.json();
+            console.log(data); //Testing only
+            displayResults(data); // uncomment when ready
+        } else {
+            throw Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function displayResults(data) {
+    currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+    const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    let desc = data.weather[0].description;
+    weatherIcon.setAttribute("src",iconsrc);
+    weatherIcon.setAttribute("alt","weather icon");
+    captionDesc.textContent =`${desc}`;
+}
+
+apiFetch();
