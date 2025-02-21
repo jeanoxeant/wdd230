aa9944b06a5a4ccb66018df49ed4d4c40f5dda47
jeanoxeant <oxe24001@byui.edu>
2025-02-21 00:24:47 -0400

    update discovery web page

--- chamber/scripts/members.js
+++ chamber/scripts/members.js
@@ -1,42 +1,66 @@
-const baseURL = "https://jeanoxeant.github.io/wdd230/";
-const linksURL = "https://jeanoxeant.github.io/wdd230/chamber/data/members.json";
+const url1 = "data/members.json";
+const cards = document.querySelector("#cards");
 
-
-async function apiFetch() {
-  try {
-      const response = await fetch(linksURL);
-      const data = await response.json();
-      console.log(data);
-      displayLinks(data.companies);
-  } catch (error) {
-      console.log(error);
-  }
+async function getCompaniesData() {
+  const response = await fetch(url1);
+  const data = await response.json();
+  displayCompanies(data.companies);
 }
 
-apiFetch();
+const displayCompanies = (companies) => {
+  companies.forEach((company) => {
+    let card = document.createElement("section");
+    let bName = document.createElement("h3");
+    //let logo = document.createElement("img");
+    let phone = document.createElement("p");
+    let industry = document.createElement("p");
+    let address = document.createElement("p");
+    
+    let website = document.createElement("a");
+    website.setAttribute("href", `https://${company.website}`);
+    website.textContent = company.website;
+    let membership = document.createElement("p");
 
-const displayLinks = (data) => {
-  const cards = document.querySelector('#list'); // Assuming you have a div with id 'links-list' to append links
-  data.forEach((week) => {
-      let card = document.createElement('div');
-      let weeks = document.createElement('section');
+    const logo = document.createElement("img");
+    logo.setAttribute("src", company.logo);
+    logo.setAttribute("alt", `Logo of ${company.name}`);
+    logo.setAttribute("loading", "lazy");
+    logo.setAttribute("width", "75");
+    logo.setAttribute("height", "auto");
 
-      weeks.textContent = `Week ${week.week}: `;
+    
 
-      week.links.forEach((link, index) => {
-          let listItem = document.createElement('li');
-          let anchor = document.createElement('a');
-          anchor.href = link.url;
-          anchor.textContent = link.title;
-          listItem.appendChild(anchor);
-          if (index < week.links.length - 1) {
-              // Add pipe character between links, except for the last one
-              listItem.appendChild(document.createTextNode(' | '));
-          }
-          weeks.appendChild(listItem);
-      });
+    bName.textContent = `${company.name}`;
+    address.textContent = `${company.address}`;
+    phone.textContent = `${company.phone}`;
+    website.setAttribute("href", `${company.url}`);
+    website.textContent = company.url;
+    website.setAttribute("target", "_blank");
+    membership.textContent = `${company.membership} Membership`;
+    
 
-      card.appendChild(companies);
-      cards.appendChild(card);
+    card.appendChild(logo);
+    card.appendChild(bName);
+    card.appendChild(address);
+    card.appendChild(phone);
+    card.appendChild(website);
+    cards.appendChild(card);
   });
+};
+getCompaniesData();
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+  display.classList.add("grid-directory");
+  display.classList.remove("list-directory");
+});
+
+listbutton.addEventListener("click", showList);
+
+function showList() {
+  display.classList.add("list-directory");
+  display.classList.remove("grid-directory");
 }
